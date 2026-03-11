f81284f6b4305887b3ae9305746ba3f39920b577
edroibarrala <edroibarrala@ittepic.edu.mx>
2026-03-10 20:33:05 -0700

    Limpieza de archivos para pruebas

--- vick_ferre_app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ vick_ferre_app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,17 +5,12 @@
 import FlutterMacOS
 import Foundation
 
-<<<<<<< HEAD
 import cloud_firestore
 import firebase_core
+import mobile_scanner
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
-=======
-import mobile_scanner
-
-func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   MobileScannerPlugin.register(with: registry.registrar(forPlugin: "MobileScannerPlugin"))
->>>>>>> 8c97cad7f9a9f4c29cefa59473e5892babaec556
 }
